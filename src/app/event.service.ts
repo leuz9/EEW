835a9d82e4ba460a5a71fcc348e0420831d939b2
Roland Niokhor Faye <leuzleuz9@gmail.com>
2019-03-14 08:21:54 +0000

    to deploy

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -9,21 +9,26 @@ export class EventService {
   constructor(private http: HttpClient) {}
 
   addEvent(event) {
-    return this.http.post('http://localhost:3000/api/v1/event', event);
+    /* return this.http.post('http://localhost:3000/api/v1/event', event); */
+    return this.http.post('https://edacy-api-prod.herokuapp.com/api/v1/event', event);
   }
 
   getEvents() {
-    return this.http.get('http://localhost:3000/api/v1/event');
+    /* return this.http.get('http://localhost:3000/api/v1/event'); */
+    return this.http.get('https://edacy-api-prod.herokuapp.com/api/v1/event');
   }
   getPublishedEvents() {
-    return this.http.get('http://localhost:3000/api/v1/event/published');
+    /* return this.http.get('http://localhost:3000/api/v1/event/published'); */
+    return this.http.get('https://edacy-api-prod.herokuapp.com/api/v1/event/published');
   }
   getEvent(id) {
-    return this.http.get(`http://localhost:3000/api/v1/event/${id}`);
+    /* return this.http.get(`http://localhost:3000/api/v1/event/${id}`); */
+    return this.http.get(`https://edacy-api-prod.herokuapp.com/api/v1/event/${id}`);
   }
 
   editEvent(event) {
-    return this.http.post(`http://localhost:3000/api/v1/event/${event._id}`, event);
+    /* return this.http.post(`http://localhost:3000/api/v1/event/${event._id}`, event); */
+    return this.http.post(`https://edacy-api-prod.herokuapp.com/api/v1/event/${event._id}`, event);
   }
 
 }
